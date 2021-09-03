47b8b02104d272d7622315741f81d33a028f3532
siulonairam <luism.riverob26@gmail.com>
2021-09-02 21:35:34 -0400

    Avance 1

--- nomina.rb
+++ nomina.rb
@@ -1,6 +1,6 @@
-class docente
-    attr_accessor ():name, :lastname, :rut, :grado, :retraso, :antiguedad, :horas)
-    def initialize(nombre, lastname, rut, grado, retraso, antiguedad, horas)
+class Docente
+    attr_accessor :name, :lastname, :rut, :grado, :retraso, :antiguedad, :horas
+    def initialize(name, lastname, rut, grado, retraso, antiguedad, horas)
         @name = name
         @lastname = lastname
         @rut = rut
@@ -11,51 +11,53 @@ class docente
     end 
 
     def grade_status
-        if @grado = "Bachiller"
-            status= 40
-        elsif @grado = "Titulado"
-           status= 80
-        elsif @grado = "Maestria"
-            status= 100 
+        if @grado == "Bachiller"
+            status == 40
+        elsif @grado == "Titulado"
+           status == 80
+        elsif @grado == "Maestria"
+            status == 100 
         else
-            status= 180
+            status == 180
         end
     end
 
     def antiguedad_status
         if @antiguedad > 15 
-            status= 0.1
+            status == 1.1
         elsif @antiguedad >= 11 or @antiguedad <= 15
-           status= 0.07
+           status == 1.07
         elsif @antiguedad >= 7 or @antiguedad < 11
-            status= 0.05
+            status == 1.05
         elsif @antiguedad >= 4 or @antiguedad < 7
-            status= 0.04
+            status == 1.04
         else
-            status= 0.03
+            status == 1.03
         end
     end
 
     def retraso_status
         if @retraso == 0 
-            status= 120
+            status = 120
         elsif @retraso >= 1 or @retraso <= 5
-           status= 80
+           status = 80
         elsif @retraso >= 6 or @retraso <= 10
-            status= 40
+            status = 40
         elsif @retraso >= 11 or @retraso <= 15
-            status= 0
+            status = 0
         else
-            status= -40
+            status = -40
         end
     end
+     
+    def mensual (total)
+        total = ((@grado * @horas) * @antiguedad) + @retraso
+     end
+end
 
-    class sueldo
-       
-        def mensual
+profe1=Docente.new("Enrique", "Melgarejo", "25654787", "Bachiller", 11, 0, 72)
+#profe2=Docente.new("Richar", "Lujano", "2555555", "Titulado", 11, 15, 78)
 
-            total = (@grado_status * @horas) * @antiguedad_status) + @retraso_status
+puts "#{@name} "
 
-        end
 
-        
